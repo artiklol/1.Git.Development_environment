dae8710f12e7096122595f69f80da74ddf3ec2ee
Artem Sulzhenko <artiklol2012@gmail.com>
2022-12-08 22:19:24 +0300

    ADD project

---
+++ task_1/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  task_1
+//
+//  Created by Artem Sulzhenko on 06.12.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
